3c6826503c1e62767fb220f3a1e1c173259316dd
nguyenhuubao20 <9.10nguyenhuubao@gmail.com>
2024-10-13 20:27:51 +0700

    fix

--- obj/Debug/net6.0/TestDeploy.AssemblyInfo.cs
+++ obj/Debug/net6.0/TestDeploy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestDeploy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9955dfbfe79084664af554f4eb19514624d9495")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+657ef7ce5ee32ebcfb772936e1e611e12a0f08a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestDeploy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestDeploy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
