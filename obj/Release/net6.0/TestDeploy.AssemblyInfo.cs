ca64bbe4550576ee8dee40cb6ff50b6e6d3f64dd
nguyenhuubao20 <9.10nguyenhuubao@gmail.com>
2024-10-13 18:48:50 +0700

    up

--- obj/Release/net6.0/TestDeploy.AssemblyInfo.cs
+++ obj/Release/net6.0/TestDeploy.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestDeploy")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26a6cac156ae2671fbe200fa67034a52b9f4d502")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestDeploy")]
